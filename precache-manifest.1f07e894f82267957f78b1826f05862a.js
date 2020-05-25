9fe6a3d39b0b1c488a2f420982a013845f0c7f69
André Lopes <lopes.andree@gmail.com>
2020-05-25 01:37:29 -0300

    Updates

---
+++ precache-manifest.1f07e894f82267957f78b1826f05862a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fab14db485ea2ba11e9cf83b1126b17",
+    "url": "/responsive-components/index.html"
+  },
+  {
+    "revision": "932a8ea55f6440140d1d",
+    "url": "/responsive-components/static/css/main.c92ace5f.chunk.css"
+  },
+  {
+    "revision": "00033a21864b2275c864",
+    "url": "/responsive-components/static/js/2.06d1fcd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/responsive-components/static/js/2.06d1fcd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932a8ea55f6440140d1d",
+    "url": "/responsive-components/static/js/main.9809494b.chunk.js"
+  },
+  {
+    "revision": "76e8750eec3750df60a0",
+    "url": "/responsive-components/static/js/runtime-main.222aa691.js"
+  },
+  {
+    "revision": "d7df36109bd02c84c2f47ff8509f2a45",
+    "url": "/responsive-components/static/media/background2.d7df3610.jpg"
+  },
+  {
+    "revision": "1f3ae97271c177e68fae6c16a7b71a7d",
+    "url": "/responsive-components/static/media/cart.1f3ae972.svg"
+  },
+  {
+    "revision": "36d74decc9e1c12667070fed3f287034",
+    "url": "/responsive-components/static/media/headphone.36d74dec.svg"
+  },
+  {
+    "revision": "8643439e9ea2b6a086d8dbfbe4b8e743",
+    "url": "/responsive-components/static/media/phone.8643439e.svg"
+  },
+  {
+    "revision": "282d00eafadab6c9aeec1b335f375d8c",
+    "url": "/responsive-components/static/media/ringing-phone.282d00ea.svg"
+  },
+  {
+    "revision": "f1e23c33f544f93c8df221998d936e33",
+    "url": "/responsive-components/static/media/speech-balloon.f1e23c33.svg"
+  }
+]);
